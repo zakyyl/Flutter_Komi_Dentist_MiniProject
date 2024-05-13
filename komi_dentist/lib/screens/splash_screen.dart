608a02c84d62d7fc7e179f445aba58636f493317
zakyyl <zaky24112003@gmail.com>
2024-05-14 01:38:08 +0700

    ✨ feat: added splash screen

---
+++ komi_dentist/lib/screens/splash_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:komi_dentist/main.dart';
+import 'package:komi_dentist/screens/login_screen.dart';
+import 'home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  _navigateToHome() async {
+    await Future.delayed(Duration(seconds: 3), () {});
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginScreen()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(  
+              child: Image.asset(
+                'assets/images/splash.png',
+                fit: BoxFit.contain  
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
